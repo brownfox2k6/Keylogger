c4a8f29bf4fbc697875ccc8ae7cc9a576a1ecab4
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2022-12-31 18:11:57 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,6 +5,7 @@ LOG_FILE = ""           # Name of log file, example, `log.txt`
 SENDER = ""             # Gmail address of the sender, example, `sender@gmail.com`
 SMTP_PASSWORD = ""      # SMTP password for the sender account. If you don't know what's SMTP password, Google is your friend :)
 RECIPIENT = ""          # Gmail address of the recipient, example `recipient@gmail.com`
+# Note: the sender and recipient addresses can be the same
 
 # Style of the html mail
 FONT_SZ = 4                    # Font size
