8eaae51932b18c664196e39b103de2eac72db9f8
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2023-01-09 21:09:02 +0700

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def get_time(day=False) -> str:
-    """
-    Get local time on computer
-    """
-    format_ = "%d %b %H:%M:%S" if day else "%H:%M:%S"
-    return datetime.now().strftime(format_)
